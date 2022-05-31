771b4239ff81116f7952515e74c0968cbdc770dc
AnnaSinko <106579388+AnnaSinko@users.noreply.github.com>
2022-05-31 10:06:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".slide-one").owlCarousel({
+      items: 1,
+      nav: true,
+      loop: true,
+  });
+
+  $(".slide-two").owlCarousel({
+      items: 3,
+      nav: true,
+      loop: true,
+  });
+    
+  $(".slide-three").owlCarousel({
+      items: 3,
+      nav: true,
+      loop: true,
+  });
+    
+});
